b3cac2a862ab08ef121e9e7495bb07b90e2353f1
tongene <ngene.christina@gmail.com>
2025-06-20 21:20:30 -0100

    e-runs-01.2-20/06

--- app/create-payments/page.tsx
+++ app/create-payments/page.tsx
@@ -2,7 +2,7 @@
 import CreatePayments from '@/components/Dashboard/CreatePayments';
 import { getPaymentsByClient } from './actions'; 
  
-const CreatePaymentPage =async ({ jobId }:{jobId:number}) => {   
+const CreatePaymentPage =async () => {   
   const paymentData=await getPaymentsByClient() 
   return (
 <><CreatePayments paymentData={paymentData||[]} /> </>
