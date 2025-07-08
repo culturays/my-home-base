9f4a7da94269a20027a39e862fa8ad834843bf11
tongene <ngene.christina@gmail.com>
2025-07-08 14:46:46 -0100

    deploy-1.1-/8/07

--- app/dashboard/create/page.tsx
+++ app/dashboard/create/page.tsx
@@ -2,13 +2,14 @@ import { createClient } from '@/utils/supabase/server';
 import { type User } from '@supabase/supabase-js' 
 import Create from '@/components/Dashboard/Create';
 import { returnProduct } from '@/app/products/return-products';
+import { redirect } from 'next/navigation';
  
 const userObj =async()=>{
     const supabase =await createClient();
     const { 
       data: { user }, 
       } = await supabase.auth.getUser();
-  
+  if(!user)redirect('/sign-in')
       return user as User
   }
 const CreateProduct= async ({searchParams}: {
@@ -17,8 +18,8 @@ const {message}=await searchParams || ''
 const {id}= await searchParams || ''
 const user = await userObj();
 const jobEdit=await returnProduct(id)
-  return (
+  return user? (
 <><Create message={message} user={user} id={id}jobEdit={jobEdit?.listedX} /> </>
-  );
+  ):<></>;
 }
 export default CreateProduct
